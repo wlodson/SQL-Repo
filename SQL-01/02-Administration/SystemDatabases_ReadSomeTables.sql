a87be52eb8bd99be82c90e2ee3cf798666876244
Daniel Wlodarczyk <daniel.wlodarczyk@gmail.com>
2020-11-16 17:41:31 +0100

    18. Bazy Systemowe - VIDEO. Skrypt na podstawie VIDEO

---
+++ SQL-01/02-Administration/SystemDatabases_ReadSomeTables.sql
@@ -0,0 +1,17 @@
+--SELECT * FROM sys.databases
+
+SELECT * FROM sys.server_principals
+
+--SELECT * FROM sys.database_principals
+
+--SELECT * FROM sys.dm_exec_connections
+
+--exec dbo.xp_fixeddrives
+
+--USE msdb
+
+--SELECT * FROM dbo.restorehistory
+
+--SELECT * FROM dbo.sysjobs
+
+--CREATE TABLE #temp (id int)
